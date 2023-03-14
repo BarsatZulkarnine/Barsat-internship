edc8a664830684e18b7225843fcdccd39a2ff484
Barsatzulkarnine <mohammedbarsatzulkarnine@gmail.com>
2023-03-14 21:05:09 +1100

    added axios, slider, skeleton state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import BrowseByCategory from "../components/home/BrowseByCategory";
 import HotCollections from "../components/home/HotCollections";
 import Landing from "../components/home/Landing";
 import LandingIntro from "../components/home/LandingIntro";
 import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
-import axios from "axios";
 
 const Home = () => {
-  const [hotCollections, setHotCollections] = useState([]);
-
-  async function getHotCollections() {
-    const {data} = await axios.get(
-      `https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`
-    );
-    console.log(data)
-    setHotCollections(data)
-  }
-
   useEffect(() => {
-    getHotCollections()
     window.scrollTo(0, 0);
   }, []);
 
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
+        <div id="top"></div> 
         <Landing />
         <LandingIntro />
         <HotCollections />
